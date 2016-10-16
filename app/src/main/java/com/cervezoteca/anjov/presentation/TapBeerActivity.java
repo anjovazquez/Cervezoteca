967349093888bc0116a2adae779a4c9dc6d97836
anjovazquez <anjo.vazquez@gmail.com>
2016-10-17 00:01:24 +0200

    Added NavigationDrawer

--- app/src/main/java/com/cervezoteca/anjov/presentation/TapBeerActivity.java
+++ app/src/main/java/com/cervezoteca/anjov/presentation/TapBeerActivity.java
@@ -1,10 +1,17 @@
 package com.cervezoteca.anjov.presentation;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.cervezoteca.anjov.domain.model.TapBeer;
 import com.cervezoteca.anjov.presentation.adapter.BeerListAdapter;
@@ -31,6 +38,11 @@ public class TapBeerActivity extends AppCompatActivity implements TapBeersView {
     @Inject
     TapBeerPresenter tapBeerPresenter;
 
+    @BindView(R.id.drawer_layout)
+    DrawerLayout mDrawerLayout;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
     @BindView(R.id.beerList)
     RecyclerView rBeerList;
 
@@ -41,6 +53,15 @@ public class TapBeerActivity extends AppCompatActivity implements TapBeersView {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tap_beers);
         ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+        final android.support.v7.app.ActionBar ab = getSupportActionBar();
+        ab.setHomeAsUpIndicator(R.drawable.ic_menu);
+        ab.setDisplayHomeAsUpEnabled(true);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        //if (navigationView != null) {
+            setupDrawerContent(navigationView);
+            installMenuListeners(navigationView);
+        //}
 
         rBeerList.setHasFixedSize(true);
         rBeerList.setLayoutManager(new LinearLayoutManager(this));
@@ -51,6 +72,50 @@ public class TapBeerActivity extends AppCompatActivity implements TapBeersView {
         tapBeersComponent.inject(this);
         tapBeerPresenter.setView(this);
         tapBeerPresenter.listTapBeers();
+    }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        menuItem.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+                        return true;
+                    }
+                });
+    }
+
+    private void installMenuListeners(NavigationView navigationView) {
+        MenuItem navBeers = navigationView.getMenu().findItem(R.id.nav_beers);
+        navBeers.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+
+                return false;
+            }
+
+        });
+
+        MenuItem navBreweries = navigationView.getMenu().findItem(R.id.nav_breweries);
+        navBreweries.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+
+                return false;
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     protected ApplicationComponent getApplicationComponent() {
